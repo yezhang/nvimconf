c237e23fd0f4853f472de3b106682a2931c323b8
yezhang <yezhang989@163.com>
2024-01-07 22:03:12 +0800

    添加了<leader>sx快捷键
    使用 <leader>sx 用于快速打开上一次搜索

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local keymap = vim.keymap
-keymap.set("i", "ii", "<Esc>")
+keymap.set("i", "jk", "<Esc>")
+
+-- 回复上一次的搜索结果
+keymap.set(
+  "n",
+  "<leader>sx",
+  require("telescope.builtin").resume,
+  { noremap = true, silent = true, desc = "resume last telescope session" }
+)
