2afa2a9ec7733c6ca56c6bdaff6f3d65a961f6ab
yezhang <yezhang989@163.com>
2024-02-07 20:35:34 +0800

    添加了JS断点调试能力，https://miguelcrespo.co/posts/debugging-javascript-applications-with-neovim/

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
-    -- { import = "lazyvim.plugins.extras.formtting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
