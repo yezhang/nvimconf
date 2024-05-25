a0676c132a871bd57993c5897cadd35139dd33c6
yezhang <yezhang989@163.com>
2024-05-25 19:11:48 +0800

    调整了编辑器为默认的相对行号

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.relativenumber = false
+-- opt.relativenumber = false
 opt.winbar = "%=%m %f %r"
