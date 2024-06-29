0db7e7a4aad3e8d46c618e1bb04e6574f94389de
yezhang <yezhang989@163.com>
2024-06-29 17:37:13 +0800

    将 live grep args 快捷键设置为 <leader>sp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,11 @@
 local keymap = vim.keymap
 keymap.set("i", "jk", "<Esc>")
 
+-- Telescope live grep 的快捷键是 <leader>sg
+-- 我们将 live grep args 的快捷键映射为类似的 <leader>sp
 keymap.set(
   "n",
-  "<leader>fp",
+  "<leader>sp",
   ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
   { noremap = true, silent = true, desc = "Live Grep (Args)" }
 )
