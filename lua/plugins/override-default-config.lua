97bea40cf1fac3937c92a0ddccbf6d703e441567
yezhang <yezhang989@163.com>
2024-06-29 19:40:56 +0800

    新增了专门用于覆盖默认配置的lua文件:override-default-config.lua

---
+++ lua/plugins/override-default-config.lua
@@ -0,0 +1,62 @@
+-- Override the configuration of the plugins
+-- This file is loaded after the plugins are loaded
+
+return {
+  -- Override the configuration of the `flash.nvim` plugin
+  -- Enable the search mode
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        search = {
+          enabled = true,
+        },
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji", "hrsh7th/cmp-cmdline" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      -- Add emoji source
+      table.insert(opts.sources, { name = "emoji" })
+
+      local cmp = require("cmp")
+
+      -- `/` cmdline setup.
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
+  -- Fix clangd offset encoding
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        clangd = function(_, opts)
+          opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+      },
+    },
+  },
+}
