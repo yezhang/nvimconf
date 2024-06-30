1dc71eada14ae0ad28ca3f80d7f28203679ae676
yezhang <yezhang989@163.com>
2024-06-30 21:49:23 +0800

    为了通过extra.lang.clangd插件自动配置clangd的offsetEncoding，移除手动配置

--- lua/plugins/override-default-config.lua
+++ lua/plugins/override-default-config.lua
@@ -14,15 +14,4 @@ return {
       },
     },
   },
-  -- Fix clangd offset encoding
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      setup = {
-        clangd = function(_, opts)
-          opts.capabilities.offsetEncoding = { "utf-16" }
-        end,
-      },
-    },
-  },
 }
