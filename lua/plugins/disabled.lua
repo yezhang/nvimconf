564a0f477cdacc3e43af9eadebff4a89f0567962
yezhang <yezhang989@163.com>
2025-03-05 20:44:15 +0800

    禁用vscode-js-debug插件，防止更新错误

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
   { "mxsdev/nvim-dap-vscode-js", enabled = false },
+  { "microsoft/vscode-js-debug", enabled = false },
   { "nvim-java/nvim-java", enabled = false },
   { "Kurama622/llm.nvim", enabled = false },
 }
